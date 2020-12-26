646957008c05dfbf1f1eb4f3095821435eb786cb
einstein <w_mansur.4563@hotmail.com>
2020-12-26 22:02:52 +0530

    sticky

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,39 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+class StickyHeader{
+    constructor(){
+        this.siteHeader=document.querySelector(".site-header")
+        this.pageSections=document.querySelectorAll(".page-section")
+        this.browserHeight=window.innerHeight
+        this.events()
+    }
+
+    events(){
+        window.addEventListener("scroll",throttle(()=>this.runOnScroll(),200))
+        window.addEventListener("resize",debounce(()=>{
+            this.browserHeight=window.innerHeight
+        },333))
+    }
+
+    runOnScroll(){
+        if(window.scrollY>60){
+            this.siteHeader.classList.add("site-header--dark")
+        }else{
+            this.siteHeader.classList.remove("site-header--dark")
+        }
+
+        this.pageSections.forEach(el=>this.calcSection(el))
+    }
+
+    calcSection(el){
+        if(window.scrollY+this.browserHeight>el.offsetTop && window.scrollY<el.offsetTop+el.offsetHeight){
+            let scrollPercent=el.getBoundingClientRect().y/this.browserHeight*100
+            if(scrollPercent<18 && scrollPercent>-0.1){
+                let matchingLink=el.getAttribute("data-matching-link")
+                document.querySelector(matchingLink).classList.add("is-current-link")
+            }
+        }
+    }
+}
+
+export default StickyHeader;
